a88ffddcb9d0d5aa8f5cb1c228fb4ef9dfcb5ded
DION RIBEIRO LARANJEIRA <dionribeiro.rr@gmail.com>
2021-08-18 22:14:36 -0400

    Initial commit

---
+++ src/main/java/com/minhasfinancas/MinhasFinancas/MinhasFinancasApplication.java
@@ -0,0 +1,14 @@
+package com.minhasfinancas.MinhasFinancas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MinhasFinancasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MinhasFinancasApplication.class, args);
+		System.out.println("Projeto Iniciado");
+	}
+
+}
